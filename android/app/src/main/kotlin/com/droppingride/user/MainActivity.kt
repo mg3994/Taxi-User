f3101edef0a88b47338bb12ced2a2ed51243384e
mg3994 <manishgautammg7@yahoo.com>
2025-02-08 12:30:07 +0530

    solved namespace problem

--- android/app/src/main/kotlin/com/droppingride/user/MainActivity.kt
+++ android/app/src/main/kotlin/com/droppingride/user/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.droppingride.user
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-class MainActivity: FlutterFragmentActivity()
+
+class MainActivity: FlutterActivity()
